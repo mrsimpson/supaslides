39fae07732963c3c0d40b2cc6aee915fb3c65cc0
Oliver Jägle <github@beimir.net>
2024-05-30 23:58:36 +0200

    Configure typescript as in vue3-starter

---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+import type { Database } from '@/types/database'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import dotenv from 'dotenv'
+
+// Load environment variables from.env file
+dotenv.config({path: ['.env.local', '.env']})
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
