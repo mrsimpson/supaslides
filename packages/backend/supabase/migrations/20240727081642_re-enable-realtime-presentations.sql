20a727241d3883def10acadcbcc0470874d2227d
Oliver Jägle <github@beimir.net>
2024-07-27 10:31:09 +0200

    Fix publications

---
+++ packages/backend/supabase/migrations/20240727081642_re-enable-realtime-presentations.sql
@@ -0,0 +1,2 @@
+ALTER PUBLICATION "supabase_realtime" ADD TABLE ONLY "public"."presentations"; 
+ALTER PUBLICATION "supabase_realtime" ADD TABLE ONLY "public"."profiles";
--- packages/frontend/playwright.config.ts
+++ packages/frontend/playwright.config.ts
@@ -107,7 +107,7 @@ export default defineConfig({
      * Use the preview server on CI for more realistic testing.
      * Playwright will re-use the local server if there is already a dev-server running.
      */
-    command: 'vite dev',
+    command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
     port: 5173,
     reuseExistingServer: !process.env.CI
   }
