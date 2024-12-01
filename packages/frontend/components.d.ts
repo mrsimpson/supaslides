128cf0dfcce56acab70b273d8b163ebf5fd81c4a
Oliver Jägle <github@beimir.net>
2024-12-01 23:15:48 +0100

    Update Global components
    Signed-off-by: Oliver Jägle <github@beimir.net>

--- packages/frontend/components.d.ts
+++ packages/frontend/components.d.ts
@@ -7,7 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    Account: typeof import('./src/components/AccountForm.vue')['default']
     AccountForm: typeof import('./src/components/AccountForm.vue')['default']
     AvatarIcon: typeof import('./src/components/AvatarIcon.vue')['default']
     BroadcastForm: typeof import('./src/components/BroadcastForm.vue')['default']
